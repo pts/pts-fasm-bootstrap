5a1831f7e2489503ba181ac72f951bd06e5f3c3a
Peter Szabo <pts@fazekas.hu>
2024-03-21 16:48:25 +0100

    bugfix: fixed octal constants for Linux open(2) syscall; this fixes permissions on newly created files

--- bootstrap_fasm.sh
+++ bootstrap_fasm.sh
@@ -7,7 +7,7 @@ comrade=../comrade
 regular=../regular
 
 #test -f "$comrade/fasm120.zip"  # 2001-11-17  No Linux support, segfault with fasm.asm and system.inc from fasm 1.37.
-test -f "$comrade/fasm130.zip"  # 2002-01-18 (file timestamps are wrong, they indicate 2001-01-18). !! Do it with something earlier.
+test -f "$comrade/fasm130.zip"  # 2002-01-18 (file timestamps are wrong, they indicate 2001-01-18). !! Do it with something earlier: fasm 1.20.
 test -f "$comrade/fasm137.zip"  # 2002-06-12 fasm 1.37 is the first with a Linux source or binary. It's UPX-compressed and it doesn't work, because it tries to use sysinfo.freeram and interpret it as bytes (too few). 
 #test -f "$comrade/fasm-1.43.tar.gz"  # First version with `format ELF executable' support, and it's already using it.
 test -f "$regular/fasm-1.73.32.tgz"  # 2023-12-04
@@ -36,10 +36,13 @@ mv tmp/SOURCE fasm-src-1.37
 rm -rf tmp
 mv fasm-src-1.37/Linux/fasm.asm fasm-src-1.37/Linux/fasm.asm.orig
 awk >fasm-src-1.37/Linux/fasm.asm <fasm-src-1.37/Linux/fasm.asm.orig '{gsub(/^include '\''..\\/, "include '\''../"); print}'
-# !! Fix octal constants in system.inc. That affects the `create:' function.
 mv fasm-src-1.37/Linux/system.inc fasm-src-1.37/Linux/system.inc.orig
+# Fix octal constants in system.inc. That affects the `create:' function.
 # Try to use at least 2.5 MiB of memory. 1 MiB is not enough. 2 MiB is enough for compiling 1.43. 2.5 MiB is enough for compilig fasm 1.73.32.
-awk >fasm-src-1.37/Linux/system.inc <fasm-src-1.37/Linux/system.inc.orig '{if(/allocate_memory:/){print"\tmov dword [buffer+14h],0x280000  ; PATCH\r"}print}'
+awk >fasm-src-1.37/Linux/system.inc <fasm-src-1.37/Linux/system.inc.orig '{
+    if (/^[ \t]*[OS]_[_A-Z0-9]+[ \t]*=/ && !/o[ \t\r]*$/) {sub(/\r?$/, "o\r")}
+    if (/allocate_memory:/){ print"\tmov dword [buffer+14h],0x280000  ; PATCH\r"}
+    print}'
 
 rm -rf tmp
 mkdir tmp
