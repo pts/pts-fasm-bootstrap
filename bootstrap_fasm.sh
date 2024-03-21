42b65349dd356bfa6bd1be3ff5da50ad4a391359
Peter Szabo <pts@fazekas.hu>
2024-03-21 16:20:11 +0100

    updated bootstrap_fasm.sh

--- bootstrap_fasm.sh
+++ bootstrap_fasm.sh
@@ -9,39 +9,11 @@ regular=../regular
 #test -f "$comrade/fasm120.zip"  # 2001-11-17  No Linux support, segfault with fasm.asm and system.inc from fasm 1.37.
 test -f "$comrade/fasm130.zip"  # 2002-01-18 (file timestamps are wrong, they indicate 2001-01-18). !! Do it with something earlier.
 test -f "$comrade/fasm137.zip"  # 2002-06-12 fasm 1.37 is the first with a Linux source or binary. It's UPX-compressed and it doesn't work, because it tries to use sysinfo.freeram and interpret it as bytes (too few). 
-test -f "$comrade/fasm-1.43.tar.gz"  # First version with `format ELF executable' support, and it's already using it.
-test -f "$regular/fasm-1.73.32.tgz"
+#test -f "$comrade/fasm-1.43.tar.gz"  # First version with `format ELF executable' support, and it's already using it.
+test -f "$regular/fasm-1.73.32.tgz"  # 2023-12-04
 
 rm -f fasm-orig-* fasm-pass?-* fasm-re-*
 rm -rf fasm-src-* tmp
-
-rm -rf tmp
-mkdir tmp
-(cd tmp && tar xzvf ../"$comrade/fasm-1.43.tar.gz" fasm/fasm fasm/source) || exit "$?"
-mv tmp/fasm/fasm fasm-orig-1.43
-chmod 755 fasm-orig-1.43  # Not needed.
-cp -a tmp/fasm/source fasm-src-1.43
-rm -rf tmp
-mv fasm-src-1.43/Linux/fasm.asm fasm-src-1.43/Linux/fasm.asm.orig
-awk >fasm-src-1.43/Linux/fasm.asm <fasm-src-1.43/Linux/fasm.asm.orig '{gsub(/^include '\''..\\/, "include '\''../"); print}'
-mv fasm-src-1.43/Linux/system.inc fasm-src-1.43/Linux/system.inc.orig
-awk >fasm-src-1.43/Linux/system.inc <fasm-src-1.43/Linux/system.inc.orig '{if(/allocate_memory:/){print"\tmov dword [buffer+14h],0x280000  ; PATCH\r"}print}'  # Try to use at least 2.5 MiB of memory. 2 MiB is not enough.
-# fasm 1.30 does not know salc.
-awk <fasm-src-1.43/Linux/fasm.asm >fasm-src-1.43/Linux/fasmb-1.30.asm '
-    BEGIN{print"salc equ setalc"}
-    {
-      if (/^[ \t]*format[ \t]/) {
-        print"program_base = 0x8048000\r";print"org program_base\r";print"use32\r"
-        print"file_header:\r";print"db 0x7F,'\''ELF'\'',1,1,1,3\r";print"rb file_header+0x10-$\r";print"dw 2,3\r";print"dd 1,start\r";print"dd program_header-file_header,0,0\r";print"dw program_header-file_header,0x20,1,0,0,0\r"
-        print"program_header:\r";print"dd 1,0,program_base,0\r";print"dd bss-program_base,program_end-program_base,7,0x1000\r"
-      } else if (/^[ \t]*entry[ \t]/) {
-      } else if (/^[ \t]*align[ \t]+4[ \t]*\r?$/) {
-        print"bss:\r";print
-      } else {
-        print
-      }
-    }
-    END{print"program_end:\r"}'
 
 rm -rf tmp
 mkdir tmp
@@ -66,7 +38,8 @@ mv fasm-src-1.37/Linux/fasm.asm fasm-src-1.37/Linux/fasm.asm.orig
 awk >fasm-src-1.37/Linux/fasm.asm <fasm-src-1.37/Linux/fasm.asm.orig '{gsub(/^include '\''..\\/, "include '\''../"); print}'
 # !! Fix octal constants in system.inc. That affects the `create:' function.
 mv fasm-src-1.37/Linux/system.inc fasm-src-1.37/Linux/system.inc.orig
-awk >fasm-src-1.37/Linux/system.inc <fasm-src-1.37/Linux/system.inc.orig '{if(/allocate_memory:/){print"\tmov dword [buffer+14h],0x200000  ; PATCH\r"}print}'  # Try to use at least 2 MiB of memory. 1 MiB is not enough.
+# Try to use at least 2.5 MiB of memory. 1 MiB is not enough. 2 MiB is enough for compiling 1.43. 2.5 MiB is enough for compilig fasm 1.73.32.
+awk >fasm-src-1.37/Linux/system.inc <fasm-src-1.37/Linux/system.inc.orig '{if(/allocate_memory:/){print"\tmov dword [buffer+14h],0x280000  ; PATCH\r"}print}'
 
 rm -rf tmp
 mkdir tmp
@@ -85,16 +58,26 @@ mv tmp/fasm/fasm fasm-orig-1.73.32
 chmod 755 fasm-orig-1.73.32  # Not needed.
 cp -a tmp/fasm/source fasm-src-1.73.32
 rm -rf tmp
-awk <fasm-src-1.73.32/Linux/fasm.asm >fasm-src-1.73.32/Linux/fasmb-1.43.asm '
-    BEGIN{print"macro align value { rb (value-1) - ($ + value-1) mod value }\r"}
+awk <fasm-src-1.73.32/Linux/fasm.asm >fasm-src-1.73.32/Linux/fasmb-1.30.asm '
     {
-      if (/^[ \t]*format[ \t]+ELF[ \t]+executable[ \t]/) {
-        print "format ELF executable\r"
+      if (/^[ \t]*format[ \t]/) {
+        print"salc equ setalc\r"
+        print"macro pushd value { push dword value }\r"
+        print"macro align value { rb (value-1) - ($ + value-1) mod value }\r"
+        print"program_base = 0x8048000\r";print"org program_base\r";print"use32\r"
+        print"file_header:\r";print"db 0x7F,'\''ELF'\'',1,1,1,3\r";print"rb file_header+0x10-$\r";print"dw 2,3\r";print"dd 1,start\r";print"dd program_header-file_header,0,0\r";print"dw program_header-file_header,0x20,1,0,0,0\r"
+        print"program_header:\r";print"dd 1,0,program_base,0\r";print"dd bss-program_base,program_end-program_base,7,0x1000\r"
+      } else if (/^[ \t]*entry[ \t]/) {
       } else if (/^[ \t]*segment[ \t]/) {
+      } else if (/^[ \t]*align[ \t]+4[ \t]*\r?$/) {
+        print"bss:\r";print
+      } else if (/^[ \t]*include[ \t]/) {
+        sub(/^include '\''..\\/, "include '\''../"); print
       } else {
         print
       }
-    }'
+    }
+    END{print"program_end:\r"}'
 
 # Compile version $2 using executable binary version $1, and then $2 by itself.
 # Input:  fasm-$1-re fasm-$2-src/
@@ -127,7 +110,6 @@ cp -a fbsasm fasm-re-bootstrap
 compile bootstrap 1.30
 #ls -l fbsasm fasm-re-1.30
 rm -f fbsasm fasm-re-bootstrap
-compile 1.30 1.43
-compile 1.43 1.73.32
+compile 1.30 1.73.32
 
 : "$0" OK.
